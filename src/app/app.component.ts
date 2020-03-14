b735bd3874ae30fb659177480de84d0089198783
Athul Nalupurakkal <athulnalupurakkal@gmail.com>
2020-03-14 22:34:57 +0530

    Added more words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { words } from "./words";
 
 @Component({
   selector: 'app-root',
@@ -9,21 +10,18 @@ export class AppComponent implements AfterViewInit {
 
   title = 'TypeFast';
   userword = "";
-  wordArray = this.GetWords();
+  wordArray = words;
   score = 0;
   timeLeft = 10;
   currentWord = this.wordArray[Math.floor(Math.random() * this.wordArray.length)];
   IsNotTimeOut = true;
   lastWord = "";
 
-  constructor() {
-  }
-
+  constructor() { }
 
   ngAfterViewInit(): void {
     window.setInterval(this.countDown.bind(this), 1000);
   }
-
 
   onValidateWord() {
     if (this.currentWord.ignoreEqual(this.userword)) {
@@ -37,35 +35,14 @@ export class AppComponent implements AfterViewInit {
       }
     }
   }
+
   countDown() {
-    // console.log(this, this.timeLeft)
     if (this.timeLeft > 0) {
       this.timeLeft = this.timeLeft - 1;
-      console.log(this.timeLeft);
     }
     else {
       this.IsNotTimeOut = false;
     }
   }
-
-
-  public GetWords(): Array<string> {
-    const words = ["Dial", "Direction", "Team", , "Worker", "Social", "Counsellors", "Helpline", "Even", "Return", "Kerala",
-      "Step", "House", "quarantine", "Decision", "However", "Proceed", "Travel", "Plan", "Inform", "Airport", "Request", "Speak"
-      , "Caller", "Father", "Mother", "Sister", "Brother", "Wife", "Husband", "Children", "Home", "Society", "Save", "Safe", "Want"
-      , "Beacuse", "Ofcourse", "Alright", "Although", "Anyway", "Country", "India", "State", "Weather", "Where", "Height", "Weight"
-      , "Race", "Right", "Wrong", "Teflon", "Teacher", "Writer", "Engineer", "Nurse", "Doctor", "Politics", "Develop", "Emapathise"
-      , "Situation", "Creation", "Outbreak", "Predicament", "Attend", "Attention", "Before", "After", "Never", "Neither", "Either"
-      , "None", "Another", "Time", "Place", "Plane", "Service", "Security", "Server", "Computer", "Television", "Camera", "Phone",
-      "Furniture", "Labourious", "Hilarious", "Arrival", "Departure", "Designer", "Architect", "Findings", "Earnigs", "Fashion"
-      , "Mask", "Persistent", "Dream", "Great", "Goat", "Good", "Food", "Sisterhood", "Collaboration", "Season", "Winter", "Summer",
-      "Today", "Yesterday", "Tomorrow", "Days", "Years", "Weekend", "Sunday", "Monday", "Teusday", "Wednesday", "Friday", "Saturday"
-      , "Often", "Growth", "Opportunity", "Technology", "Something", "Someone", "Special", "Knowledge", "Transfer", "Building",
-      "Reality", "Women", "Emotion", "Rejection", "Acceptance", "Criteria", "Problem", "Solution", "Correction", "Direction", "Adherence"
-      , "Excited", "Extrovert", "Introvert", "Provider", "Subscriber", "Elasticated", "Instagram", "Pharmaceutical", "Manufacturers"
-      , "Presidential", "Tournaments"]
-    return words;
-  }
-
 
 }
